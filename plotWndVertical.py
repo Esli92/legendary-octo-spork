8aa710178d9d4d3be6066cf188d1541b241eb2f5
Esli_Jurado <moca92@gmail.com>
2017-06-05 11:49:31 -0500

    added PBL function

---
+++ plotWndVertical.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+import cartopy.crs as crs
+from cartopy.feature import NaturalEarthFeature
+from netCDF4 import Dataset
+
+from wrf import to_np, getvar, CoordPair, vertcross
+
+# Open the NetCDF file
+filename = "../../../salidas/times.nc"
+ncfile = Dataset(filename)
+
+# Extract the model height and wind speed
+z = getvar(ncfile, "p")
+wspd =  getvar(ncfile, "uvmet_wspd_wdir", units="m/s")[0,:]
+
+# Create the start point and end point for the cross section
+start_point = CoordPair(lat=19.3821, lon=-99.7940)
+end_point = CoordPair(lat=19.1557, lon=-99.5771)
+
+# Compute the vertical cross-section interpolation.  Also, include the lat/lon
+# points along the cross-section.
+wspd_cross = vertcross(wspd, z, wrfin=ncfile, start_point=start_point, end_point=end_point,
+                       latlon=True, meta=True)
+
+# Create the figure
+fig = plt.figure(figsize=(12,6))
+ax = plt.axes()
+
+# Make the contour plot
+wspd_contours = ax.contourf(to_np(wspd_cross), cmap=get_cmap("jet"))
+
+# Add the color bar
+plt.colorbar(wspd_contours, ax=ax)
+
+# Set the x-ticks to use latitude and longitude labels.
+coord_pairs = to_np(wspd_cross.coords["xy_loc"])
+x_ticks = np.arange(coord_pairs.shape[0])
+x_labels = [pair.latlon_str(fmt="{:.2f}, {:.2f}") for pair in to_np(coord_pairs)]
+ax.set_xticks(x_ticks[::20])
+ax.set_xticklabels(x_labels[::20], rotation=45, fontsize=8)
+
+# Set the y-ticks to be height.
+vert_vals = to_np(wspd_cross.coords["vertical"])
+v_ticks = np.arange(vert_vals.shape[0])
+ax.set_yticks(v_ticks[::20])
+ax.set_yticklabels(vert_vals[::20], fontsize=8)
+
+# Set the x-axis and  y-axis labels
+ax.set_xlabel("Latitude, Longitude", fontsize=12)
+ax.set_ylabel("Height (m)", fontsize=12)
+
+plt.title("Vertical Cross Section of Wind Speed (m/s)")
+
+plt.show()
